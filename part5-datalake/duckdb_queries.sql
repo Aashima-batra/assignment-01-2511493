3581ca2b6c55784627a286e721d0f7eb5ab16c70
Aashima-batra <aashimabatra2244@gmail.com>
2026-03-19 20:42:26 +0530

    Refactor SQL queries for DuckDB compatibility

--- part5-datalake/duckdb_queries.sql
+++ part5-datalake/duckdb_queries.sql
@@ -1,35 +1,57 @@
--- Q1: List all customers along with the total number of orders they have placed
-SELECT c.customer_id, c.name, COUNT(o.order_id) AS total_orders
-FROM read_csv_auto('datasets/customers.csv') c
-LEFT JOIN read_json_auto('datasets/orders.json') o
+## Q1: List all customers along with the total number of orders they have placed
+
+duckdb.query("""
+
+SELECT
+   c.name,
+    COUNT(o.order_id) AS total_orders
+FROM read_csv_auto('/customers.csv') c
+LEFT JOIN read_json_auto('/orders.json') o
 ON c.customer_id = o.customer_id
 GROUP BY c.customer_id, c.name;
+""").df()
+
 
 
 -- Q2: Find the top 3 customers by total order value
-SELECT c.name, SUM(o.total_amount) AS total_value
-FROM read_csv_auto('datasets/customers.csv') c
-JOIN read_json_auto('datasets/orders.json') o
+
+duckdb.query("""
+  
+SELECT 
+  c.name,
+  SUM(o.total_amount) AS total_value
+FROM read_csv_auto('/customers.csv') c
+JOIN read_json_auto('/orders.json') o
 ON c.customer_id = o.customer_id
 GROUP BY c.name
 ORDER BY total_value DESC
 LIMIT 3;
+ """).df()
+  
 
 
--- Q3: List all products purchased by customers from Bangalore
-SELECT p.product_name
-FROM read_csv_auto('datasets/customers.csv') c
-JOIN read_json_auto('datasets/orders.json') o
+-- Q3: List all products purchased by customers from Bangalore 
+  
+duckdb.query("""  
+SELECT DISTINCT
+   p.product_name
+FROM read_csv_auto('/customers.csv') c
+JOIN read_json_auto('/orders.json') o
 ON c.customer_id = o.customer_id
-JOIN read_parquet('datasets/products.parquet') p
-ON o.product_id = p.product_id
-WHERE c.city = 'Bangalore';
+JOIN read_parquet('/products.parquet') p
+ON o.order_id = p.order_id
+WHERE c.city = 'Bangalore'
+""").df()
 
 
 -- Q4: Join all three files to show customer name, order date, product name, and quantity
-SELECT c.name, o.order_date, p.product_name, o.quantity
-FROM read_csv_auto('datasets/customers.csv') c
-JOIN read_json_auto('datasets/orders.json') o
+
+duckdb.query("""
+SELECT
+  c.name, o.order_date, p.product_name, o.status
+FROM read_csv_auto('/customers.csv') c
+JOIN read_json_auto('/orders.json') o
 ON c.customer_id = o.customer_id
-JOIN read_parquet('datasets/products.parquet') p
-ON o.product_id = p.product_id;
+JOIN read_parquet('/products.parquet') p
+ON o.order_id = p.order_id;
+""").df()
